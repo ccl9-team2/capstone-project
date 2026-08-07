8bb0125ec5f34bad7021071ae8c41e86420d5c0f
Lindsey Armstrong <linmicarm@gmail.com>
2026-08-07 19:13:32 -0400

    added payments and expense splits apis

---
+++ apps/backend/server/routes/payments.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+import {
+  getPayments,
+  getPaymentById,
+  createPayment,
+  updatePayment,
+  deletePayment
+} from "../controllers/paymentController.js";
+
+const router = express.Router();
+
+router.get("/", getPayments);
+router.get("/:id", getPaymentById);
+
+router.post("/", createPayment);
+
+router.put("/:id", updatePayment);
+
+router.delete("/:id", deletePayment);
+
+export default router;
